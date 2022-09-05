9af654b96f9edfd39ad9621ae31a029e3a8d105f
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2022-09-05 09:52:03 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+
+    student me = new student();
+
+    me.surname = "Poquiz";
+    me.firstname = "John Darrel";
+    me.middleinitial = 'C';
+    me.dateofbirth = "06/29/2003";
+    me.studentnumber = 2022102042;
+    me.studentemailaddress = "poquizjc@student.national-u.edu.ph";
+    me.iAmAwesome = true;
+    me.sayMyStudentNumber();
+    me.sayMyEmailAddress();
+    me.amIAwesome();
+    }
+}
